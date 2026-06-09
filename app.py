1710dca960645a600a680df3aa63c3d1ce8b9e7b
FundLock <tomkin.dan@gmail.com>
2026-06-09 00:18:59 -0400

    Update app.py polished app V6

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="FundLock", layout="centered")
 st.title("MCA FundLock 🔒")
 
 st.write(
-    "Protect sensitive merchant data. Seamless Application Transfer. Deliver lender-ready packages."
+    "Protect sensitive merchant data. Seamless application transfer. Deliver lender-ready packages."
 )
 
 st.markdown(
